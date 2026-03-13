40141497b26ef4901af6c0d12dc1fcf3a42b4c22
ShivangBokolia <sbokolia@gmail.com>
2026-03-13 15:24:52 -0500

    Updated the base url for portfolio

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: "https://github.com/ShivangBokolia/AIML-500",
+    base: "/AIML-500/",
 });
