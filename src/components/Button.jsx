a108deb4d30508b49353ac6abab9e230b55fc896
ShivangBokolia <sbokolia@gmail.com>
2026-03-15 12:55:10 -0500

    button fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,10 +6,10 @@ const Button = (props) => {
         return (
             <>
                 <Link
-                    className="p-0.5 rounded-full from-[#0C1B1D] to-highlight bg-gradient-to-r shadow-2xl"
+                    className="p-0.5 rounded-full from-[#0C1B1D] to-highlight bg-gradient-to-r shadow-2xl flex flex-row"
                     to={`/${props.page}`}
                 >
-                    <span className="block text-white px-4 py-2 font-semibold rounded-full bg-background-green hover:bg-transparent duration-300">
+                    <span className="w-full text-center block text-white px-4 py-2 font-semibold rounded-full bg-background-green sm:hover:bg-transparent duration-300">
                         {props.text}
                     </span>
                 </Link>
